0ba29f0c444b894ba84b3ddc1a18758414328ec4
Roberto <roberto_junior_sousa@hotmail.com>
2019-11-22 23:47:20 -0300

    configuração para envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f66e85922aa171',
+    pass: '18a3e137d69e7e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
