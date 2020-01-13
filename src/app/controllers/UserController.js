e7b4e73faf321f00f6ae8c0ba6aafa1fb76cc8c3
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-13 15:08:42 -0300

    adicionado await no método update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -71,7 +71,7 @@ class UserController {
 
     await user.update(req.body);
 
-    const { id, name, avatar } = User.findByPk(req.userId, {
+    const { id, name, avatar } = await User.findByPk(req.userId, {
       include: [
         {
           model: File,
